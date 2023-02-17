631bfbe2c2d562498d08827adb79b77b5eced65e
Quentin Honnart <quentin.honnart@gmail.com>
2023-02-17 15:02:51 +0100

    add padding on responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <main>
           <Header />
           <div className="max-w-[1440px] mx-auto">
-              <div className={"max-w-[1280px] h-fit mx-auto"}>
+              <div className={"max-w-[1280px] h-fit mx-auto px-8"}>
 
               </div>
           </div>
