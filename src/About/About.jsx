1a54233c1c6bb5f64b11e6ded1e846fc4e540613
rolpf <jiroxane@hotmail.fr>
2023-03-14 20:34:53 +0100

    text modification

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,9 +2,9 @@ import AboutCard from "./Components/AboutCard";
 
 export default function About() {
     return (
-        <div href="#about">
+        <div className="py-8" href="#about">
             <p className={"flex justify-center text-2xl font-bold my-4"}>Qui sommes-nous ?</p>
-            <p className={"flex text-center justify-center my-4"}><b>Nom de l'agence</b>, Texte expliquant qui nous sommes ainsi que nos valeurs ou je ne sais quoicoubeh</p>
+            <p className={"flex text-center justify-center my-4"}><b>Nom de l'agence</b>, c'est 5 étudiants en licence professionnelle créaweb à l'IUT de Lens. Passionés, curieux, polyvalents, nous sommes ouverts à la découverte de nouveaux projets.</p>
             <div className={"lg:grid lg:grid-cols-3 justify-center "}>
                 <AboutCard image="https://i.pinimg.com/474x/b0/1a/43/b01a43908d262eeb65208eccdf31aac9.jpg" name="Quentin" role="My CEO" />
                 <AboutCard image="https://i.pinimg.com/474x/b0/1a/43/b01a43908d262eeb65208eccdf31aac9.jpg" name="Laurine" role="My CEO" />
