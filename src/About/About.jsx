5f256ac03b0e757f7806da7ebc688c4f8311ab5d
rolpf <jiroxane@hotmail.fr>
2023-06-01 11:04:14 +0200

    remove /public from src of image

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,11 +6,11 @@ export default function About() {
             <p className={"flex justify-center text-center text-2xl font-bold my-4 underline decoration-orange-400"}>Qui sommes-nous ?</p>
             <p className={"text-center justify-center my-4"}>L'agence<span className="font-bold"> Fier de moi</span>, c'est 5 étudiants en licence professionnelle créaweb à l'IUT de Lens. Passionés, curieux, polyvalents, nous sommes ouverts à la découverte de nouveaux projets.</p>
             <div className={"flex lg:items-center flex-wrap justify-center "}>
-                <AboutCard image="/public/photos/quentin.png" name="Quentin" role="Lead développeur fullstack" />
-                <AboutCard image="/public/photos/laurine.jpg" name="Laurine" role="Designer UI/UX" />
-                <AboutCard image="/public/photos/kahina.jpg" name="Kahina" role="Communication" />
-                <AboutCard image="/public/photos/theo.jpg" name="Théo" role="Designer UI/UX"/>
-                <AboutCard image="/public/photos/roxane.jpg" name="Roxane" role="Développement front"/>
+                <AboutCard image="/photos/quentin.png" name="Quentin" role="Lead développeur fullstack" />
+                <AboutCard image="/photos/laurine.jpg" name="Laurine" role="Designer UI/UX" />
+                <AboutCard image="/photos/kahina.jpg" name="Kahina" role="Communication" />
+                <AboutCard image="/photos/theo.jpg" name="Théo" role="Designer UI/UX"/>
+                <AboutCard image="/photos/roxane.jpg" name="Roxane" role="Développement front"/>
             </div>
         </div>
     )
--- src/Realisations/Realisations.jsx
+++ src/Realisations/Realisations.jsx
@@ -19,26 +19,26 @@ export default function Realisations() {
             title="Site web pour une alpiniste"
             desc="Création d'une maquette à partir d'un brief client, puis développement du site web avec Wordpress et Themosis."
             link = "https://christinevogondy.ch"
-            image="/public/photos/christine-vogondy.png"/></SwiperSlide>
+            image="/photos/christine-vogondy.png"/></SwiperSlide>
       <SwiperSlide>
             <RealisationCard 
             title="Site web - Portfolio d'étudiant"
             desc="Création d'un site web présentant le portfolio d'un étudiant, avec Wordpress et Themosis."
             link="#"
-            image="/public/photos/fifteen-prod.png"/>
+            image="/photos/fifteen-prod.png"/>
       </SwiperSlide>
       <SwiperSlide>
             <RealisationCard 
             title="Refonte d'un site web sous Wix"
             desc="Refonte graphique et ergonomique d'un site web existant"
             link="https://www.elyetsespepites.fr/"
-            image="/public/photos/ely_wix.png"/>
+            image="/photos/ely_wix.png"/>
       </SwiperSlide>
       <SwiperSlide>
             <RealisationCard 
             title="Création de supports de communication"
             desc="Création de supports de communication pour les réseaux sociaux et carte de visite"
-            image="/public/photos/ely.png"/>
+            image="/photos/ely.png"/>
       </SwiperSlide>
     </Swiper> 
         </div>
